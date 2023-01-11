2f56339bf90a77b748564d8016e2542358ec77a7
Afeef-Z <afeef.engr.zaman@gmail.com>
2023-01-11 09:31:01 -0800

    Added functionality to read .csv file

--- Ligo_Sports_Assessment/server.js
+++ Ligo_Sports_Assessment/server.js
@@ -1,10 +1,32 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const fs = require("fs");
+const { parse } = require("csv-parse");
+const ObjectsToCsv = require("objects-to-csv");
 app.use(express.json());
 
-app.get("/games/:playerId", async (req, res) => {
-  res.json("prototype of the get request");
+const csvFile = "./data.csv";
+
+const promise = new Promise((resolve, reject) => {
+  const arr = [];
+  fs.createReadStream(csvFile)
+    .pipe(parse({ delimiter: ",", columns: true, trim: true }))
+    .on("data", (row) => {
+      arr.push(row);
+    })
+    .on("end", resolve(arr))
+    .on("error", reject);
+});
+
+app.get("/games/:playerId", async (req, res) =>  {
+  await promise.then(function (arr) {
+    if (arr.length < 1) {
+      res.status(404);
+    } else {
+      res.json(arr);
+    }
+  });
 });
 
 app.listen(port, () => {
